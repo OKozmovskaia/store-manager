d6b90181d5b7de2cf989ce16190ae2637ab1e6ea
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-04-15 18:43:14 +0200

    Add redirect and deep link to project

--- components/screens/InstagramAuth.js
+++ components/screens/InstagramAuth.js
@@ -4,13 +4,29 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 import config from '../../config.json';
 
+const getCodeForAccessToken = async() => {
+  await fetch(
+    'https://api.instagram.com/oauth/authorize',
+    {
+      method: 'GET',
+      client_id: config.ID_BUSINESS_ACCOUNT_INSTAGRAM,
+      redirect_uri: config.REDIRECT_URL,
+      scope: 'user_profile,user_media',
+      response_type: 'code'
+    })
+    .then(res => res.text())
+    .then(json => console.log(json))
+    .catch(er => console.log(er))
+}
+
 export default function InstagramAuth() {
   return (
     <View>
       <Icon.Button
         name="instagram"
+        onPress={getCodeForAccessToken}
       >
-        <Text style={{ fontFamily: 'Arial', fontSize: 15 }}>
+        <Text>
           Get my feed from Instagram
         </Text>
       </Icon.Button>
