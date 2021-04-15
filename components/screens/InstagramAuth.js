29216b0665e85bd6337213177983f81c9daa0955
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-04-15 19:55:33 +0200

    Add config for auth

--- components/screens/InstagramAuth.js
+++ components/screens/InstagramAuth.js
@@ -5,6 +5,38 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import config from '../../config.json';
 
 const getCodeForAccessToken = async() => {
+  const [authCode, setAuthCode] = useState('');
+  const [token, setToken] = useState('')
+
+  const fetchConfigs = {
+    getAuthCode: {
+      issuer: 'https://api.instagram.com/oauth/authorize',
+      clientId: config.ID_BUSINESS_ACCOUNT_INSTAGRAM,
+      redirectUrl: config.REDIRECT_URL,
+      scopes: 'user_profile,user_media',
+      responseTypes: 'code'
+    },
+  
+    getShortLivedToken: {
+      issuer: 'https://api.instagram.com/oauth/access_token',
+      additionalParameters: {
+        code: `${authCode}`
+      },
+      client_id: config.ID_BUSINESS_ACCOUNT_INSTAGRAM,
+      client_secret: config.APP_SECRET,
+      redirect_uri: config.REDIRECT_URL,
+      grantTypes: 'authorization_code'
+    },
+    
+    getLongLivedToken: {
+      issuer: 'https://graph.instagram.com/access_token',
+      client_secret: config.APP_SECRET,
+      grantTypes: 'ig_exchange_token',
+      additionalParameters: {
+        access_token: `${token}`
+      }
+    }
+  }
   await fetch(
     'https://api.instagram.com/oauth/authorize',
     {
