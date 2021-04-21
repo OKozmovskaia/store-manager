fccb0594f92223e67ff91e0a7ad0cd4f00938cea
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-04-21 15:15:52 +0200

    Style New User Screen

--- components/screens/InstagramAuth.js
+++ components/screens/InstagramAuth.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, Linking, StyleSheet } from 'react-native';
+import { View, Text, Linking, StyleSheet, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import config from '../../config.json';
 
-export default function InstagramAuth() {
+export default function InstagramAuth({ navigation: { navigate } }) {
 
   const enteredURL = `https://www.instagram.com/oauth/authorize?client_id=${config.ID_BUSINESS_ACCOUNT_INSTAGRAM}&redirect_uri=${config.REDIRECT_URL}&scope=user_profile,user_media&response_type=code`;
 
@@ -23,6 +23,16 @@ export default function InstagramAuth() {
           Get my feed from Instagram
         </Text>
       </Icon.Button>
+
+      <Button
+        onPress={() => navigate('NewUser')}
+        title='Go to screen for New User'
+      />
+
+      <Button
+        onPress={() => navigate('ExistUser')}
+        title='Go to screen for Exist User'
+      />
     </View>
   );
 }
@@ -32,7 +42,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 24,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around'
   },
   instagramAuthButton: {
     borderRadius: 6,
--- components/screens/NewUser.js
+++ components/screens/NewUser.js
@@ -1,10 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function NewUser () {
   return(
-    <View>
-      <Text>New User Screen</Text>
+    <View style={styles.newUserScreenContainer}>
+      <Text
+      style={styles.newUserScreenText}
+      >Would you like to join to our loyalty program?</Text>
+      <Icon name="gift" style={styles.newUserScreenIcon}/>
+      <View>
+        <TextInput
+          style={styles.newUserScreenInput}
+          defaultValue="Enter your name"
+        />
+        <TextInput
+          style={styles.newUserScreenInput}
+          defaultValue="Enter your phone"
+        />
+        <Button
+          title='Join to loyalty program'
+          color='#09b83e'
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  newUserScreenContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    paddingHorizontal: 20
+  },
+  newUserScreenText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  newUserScreenIcon: {
+    fontSize: 150,
+    color: '#09b83e',
+  },
+  newUserScreenInput: {
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: 300,
+    borderColor: '#a9a9a9',
+    borderRadius: 5,
+    borderWidth: 1
+  }
+})
